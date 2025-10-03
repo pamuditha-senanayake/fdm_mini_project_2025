25368be7518f1f4c08a9fe0da00ab0fe20725ecf
pamuditha <pamudithasenanayake@gmail.com>
2025-10-03 22:13:21 +0530

    netlify anf render error fixing 1

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
 import SalesForecast from "../components/SalesForecast";
 import PromotionPredict from "../components/PromotionPredict";
